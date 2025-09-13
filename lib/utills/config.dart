3f1355eb033fd75143e938701958635f76cefa95
liuyuqi-cnb <lyq.me@qq.com>
2025-09-13 21:11:55 +0800

    change baseurl

--- lib/utills/config.dart
+++ lib/utills/config.dart
@@ -1,6 +1,6 @@
 // ------------Use This Url For Esim Demo----------------
-const String baseUrl = "https://esim.your.com/api";
-const String imageBaseUrl = "https://esim.your.com";
+const String baseUrl = "https://y-esim.com/api";
+const String imageBaseUrl = "https://y-esim.com";
 
 // ------------Use This Url for PlayStore Only----------------
 // const String baseUrl = "https://esimtel.app/api";
